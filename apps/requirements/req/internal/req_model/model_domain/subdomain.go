19516d5b318e98250251609a5933e033b6280573
Justin Scott <justin.james.scott@gmail.com>
2026-01-16 20:23:26 -0800

    Fixign children of subdomain in model.

--- apps/requirements/req/internal/req_model/model_domain/subdomain.go
+++ apps/requirements/req/internal/req_model/model_domain/subdomain.go
@@ -16,10 +16,10 @@ type Subdomain struct {
 	Details    string // Markdown.
 	UmlComment string
 	// Children
-	Generalizations   []model_class.Generalization // Generalizations for the classes and use cases in this subdomain.
-	Classes           []model_class.Class          // Classes in this subdomain.
-	UseCases          []model_use_case.UseCase     // Use cases in this subdomain.
-	ClassAssociations []model_class.Association    // Associations between classes in this subdomain.
+	Generalizations   map[identity.Key]model_class.Generalization // Generalizations for the classes and use cases in this subdomain.
+	Classes           map[identity.Key]model_class.Class          // Classes in this subdomain.
+	UseCases          map[identity.Key]model_use_case.UseCase     // Use cases in this subdomain.
+	ClassAssociations map[identity.Key]model_class.Association    // Associations between classes in this subdomain.
 }
 
 func NewSubdomain(key identity.Key, name, details, umlComment string) (subdomain Subdomain, err error) {
@@ -67,23 +67,23 @@ func (s *Subdomain) ValidateWithParent(parent *identity.Key) error {
 		return err
 	}
 	// Validate all children.
-	for i := range s.Generalizations {
-		if err := s.Generalizations[i].ValidateWithParent(&s.Key); err != nil {
+	for _, gen := range s.Generalizations {
+		if err := gen.ValidateWithParent(&s.Key); err != nil {
 			return err
 		}
 	}
-	for i := range s.Classes {
-		if err := s.Classes[i].ValidateWithParent(&s.Key); err != nil {
+	for _, class := range s.Classes {
+		if err := class.ValidateWithParent(&s.Key); err != nil {
 			return err
 		}
 	}
-	for i := range s.UseCases {
-		if err := s.UseCases[i].ValidateWithParent(&s.Key); err != nil {
+	for _, useCase := range s.UseCases {
+		if err := useCase.ValidateWithParent(&s.Key); err != nil {
 			return err
 		}
 	}
-	for i := range s.ClassAssociations {
-		if err := s.ClassAssociations[i].ValidateWithParent(&s.Key); err != nil {
+	for _, classAssoc := range s.ClassAssociations {
+		if err := classAssoc.ValidateWithParent(&s.Key); err != nil {
 			return err
 		}
 	}
