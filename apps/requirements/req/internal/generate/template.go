445823c687777630deb5321f95ecdb9972b32e11
Justin Scott <justin.james.scott@gmail.com>
2026-02-24 21:53:32 -0800

    Got classes and actors linking well

--- apps/requirements/req/internal/generate/template.go
+++ apps/requirements/req/internal/generate/template.go
@@ -266,6 +266,10 @@ var _funcMap = template.FuncMap{
 		lookup := reqs.ScenarioLookup()
 		return lookup[key.String()]
 	},
+	"actor_lookup": func(reqs *req_flat.Requirements, key identity.Key) (actor model_actor.Actor) {
+		lookup := reqs.ActorLookup()
+		return lookup[key.String()]
+	},
 	"actor_classes": func(reqs *req_flat.Requirements, key identity.Key) (classes []model_class.Class) {
 		lookup := reqs.ActorClassesLookup()
 		return lookup[key.String()]
