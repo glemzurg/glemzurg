2672a689a9b5decfaad9179bb7daf84ae3ad8536
Justin Scott <justin.james.scott@gmail.com>
2026-03-05 16:38:55 -0800

    Fixing the output.

--- apps/requirements/req/internal/generate/dump_test_model_test.go
+++ apps/requirements/req/internal/generate/dump_test_model_test.go
@@ -25,7 +25,7 @@ func TestGenerateTemplates(t *testing.T) {
 
 func TestDumpTestModel(t *testing.T) {
 
-	// t.Skip("DumpTestModel is a utility for dumping the test model to a directory for manual inspection; not a real test")
+	t.Skip("DumpTestModel is a utility for dumping the test model to a directory for manual inspection; not a real test")
 
 	model := test_helper.GetTestModel()
 
